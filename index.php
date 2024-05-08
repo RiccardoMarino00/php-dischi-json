1dab0e8952aaca77661442cde87756f556b635bb
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-08 16:44:39 +0200

    Added: style

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Music Disk</title>
 </head>
 <body>
@@ -12,14 +15,18 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <div v-if="music_disks.length > 0">
-                        <div v-for="disk in music_disks">
-                            <p> {{disk.title}} </p>
-                            <p> {{disk.author}} </p>
-                            <p> {{disk.year}} </p>
-                            <img :src="disk.poster"> 
-                            <p> {{disk.genre}} </p>
-                       </div>
+                    <div v-if="music_disks.length > 0" class="container">
+                        <div class="row">
+                            <div v-for="disk in music_disks" class="music-card txt-center ">
+                                <div class="txt-center">
+                                    <img class="img-poster" :src="disk.poster">
+                                </div>
+                                <p> {{disk.title}} </p>
+                                <p class="txt-light"> {{disk.author}} </p>
+                                <p class="txt-light"> {{disk.year}} </p>
+                                <p class="txt-light"> {{disk.genre}} </p>
+                            </div>
+                        </div>
                     </div>
                     <span v-else>Non ci sono risultati</span>
                 </div>
@@ -34,3 +41,48 @@
     
 </body>
 </html>
+
+
+<style>
+
+
+
+    body{
+        background: url('./img/green-abstract-geometric-shape-background-free-vector.jpg');
+        background-size: cover;
+        font-family: "Poppins", sans-serif;
+        font-weight: 700;
+        font-style: normal;
+    }
+
+    .music-card{
+        background-color: rgba(10, 91, 12, 0.5);
+        width: 20%;
+        margin: 10px;
+        padding: 18px;
+        border-radius: 12px;
+    }
+
+    .img-poster{
+        width: 200px;
+        aspect-ratio: 1/1;
+        border-radius: 12px;
+        filter: saturate(2);
+    }
+
+    .row{
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .txt-center{
+        text-align: center;
+    }
+
+    .txt-light{
+        color: rgba(255, 255, 255, 1);
+        font-family: "Poppins", sans-serif;
+        font-weight: 300;
+        font-style: normal;
+    }
+</style>
